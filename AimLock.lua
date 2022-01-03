6b7f6230d696228f9c89db1525a8f54e67c8ffa0
xzen <93392372+xZenku@users.noreply.github.com>
2022-01-03 11:01:18 +0700

    Update AimLock.lua

--- AimLock.lua
+++ AimLock.lua
@@ -1,5 +1,5 @@
 -- // Dependencies
-local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Load.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/xZenku/Da-hood/main/b.lua"))()
 
 -- // Services
 local UserInputService = game:GetService("UserInputService")
